835b3373e02a884c591996e6636ad002ca378cf4
unknown <arfatrahman08@gmail.com>
2025-05-17 19:00:39 +0600

    unused image removed

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import ProductClientComponent from "@/components/products/products-client-component";
 import { metaDataGeneratorForNormalPage } from "@/hoook/generate-meta";
-import Image from "next/image";
 
 export const metadata = metaDataGeneratorForNormalPage("Home");
 export default function Home() {
